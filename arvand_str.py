e6a088f63339b98d89bd25cd9c7b7d3e846443a4
GulovM <147307957+GulovM@users.noreply.github.com>
2023-12-04 00:44:52 +0500

    Update arvand_str.py

--- arvand_str.py
+++ arvand_str.py
@@ -107,10 +107,7 @@ def main():
             st.success(f'Вероятность возврата кредита вовремя: {result2[0]*100:.2f}%')
             result4 = Delays_days(Loan_amount, Loan_term, Number_of_delays, Lending_stage, Gross_profit, 
                     Net_profit, Age, Region_code, Direction_of_activity, business_experience, result1)
-            if result4[0]:.2f <0:
-                st.success(f'Сколько примерно дней вы возможно просрочите: {0}')
-            else:
-                st.success(f'Сколько примерно дней вы возможно просрочите: {result4[0]:.2f}')
+            st.success(f'Сколько примерно дней вы возможно просрочите: {result4[0]:.2f}')
                                                    
 if __name__ == '__main__':
     main()
