9e1c02d0806807fac7831dc1f0e133b521869c38
GulovM <147307957+GulovM@users.noreply.github.com>
2023-12-07 23:47:06 +0500

    Update arvand_str.py

--- arvand_str.py
+++ arvand_str.py
@@ -7,75 +7,222 @@ from sklearn.preprocessing import LabelEncoder
 # Заголовок
 st.subheader('Arvand Scoring')
 
-with open("lr_model2.pkl", "rb") as pickle_in:
-    classifier = joblib.load(pickle_in)
+with open("CatBoost_model.pkl", "rb") as pickle_in:
+    classifier1 = joblib.load(pickle_in)
 
-with open("model_gb.pkl", "rb") as pickle_in:
+with open("RF_model.pkl", "rb") as pickle_in:
+    classifier2 = joblib.load(pickle_in)
+
+with open("XGB_model.pkl", "rb") as pickle_in:
+    classifier3 = joblib.load(pickle_in)
+    
+with open("days.pkl", "rb") as pickle_in:
     regression1 = joblib.load(pickle_in)
 
-with open("model_gb2.pkl", "rb") as pickle_in:
+with open("sum.pkl", "rb") as pickle_in:
     regression2 = joblib.load(pickle_in)
-
+new_data = pd.DataFrame({'Nationality': ['Узбек', 'Точик', 'Тотор', 'Рус', 'Киргиз', 'Украин', 'Другие', 'Карис',
+ 'Карачои'],
+'Filial': ['Истаравшан', 'Хучанд', 'Ч. Расулов', 'Душанбе', 'Исфара', 'Панчакент'],
+'Region': ['Шахристон', 'Гули сурх', 'Худжанд-Центр', 'Спитамен', 'Шарк', 'Мархамат',
+ 'Душанбе', 'Навкент', 'Кистакуз', 'Худжанд-Панчшанбе', 'Бустон',
+ 'Истаравшан-филиал', 'Рудаки', 'Ашт', 'Калининобод', 'Сино', 'Исфара', 'Хисор',
+ 'Зафаробод', 'Ничони', 'Вахдат', 'Мехнатобод', 'Уяс', 'Дж.Расулов', 'Конибодом',
+ 'Дусти', 'Ниёзбек','Истаравшан', 'Рогун','Гончи', 'Чашмасор', 'Нофароч',
+ 'Ободи', 'Каракчикум', 'Оббурдон', 'Куруш', 'Ворух', 'Гулякандоз', 'Некфайз',
+ 'Сомгор', 'Пунук', 'Панчакент', 'Кулканд', 'Оппон', 'Файзобод', 'Турсунзода',
+ 'Гусар', 'Равшан','Ифтихор', 'Х.Алиев', 'Ёри', 'Мучун', 'Саразм'],
+'loan_goal': ['Животноводство и переработка молока', 'Приобретение техники',
+       'Ремонт дома', 'торговля', 'Земледелие', 'Приобретение мебели',
+       'Оплата на лечение', 'Проведение мероприятий', 'Оплата поездок',
+       'Услуги', 'Переоборудование транспорта', 'Потребнужды',
+       'Оплата образования', 'Производство', 'Покупка квартиры',
+       'Потреб.другое', 'Ремонт места деятельности', 'Сельское хозяйство',
+       'Все', 'Сушка фруктов', 'Коммерческий'],
+'sector': ['Животноводство', 'Потреб Экспресс', 'Потребнужды', 'Торговля',
+       'Зеленый кредит - Печки', 'Сельхозкультура (ТАФФ)', 'Ремонт жилья',
+       'Бизнес Экспресс', 'Услуги', 'KFW - Ремонт жилья',
+       'Сельхозтехника (ТАФФ)', 'KFW - Покупка и строит-во жилья',
+       'Производство', 'Образование', 'Мигрант-бизнес 2',
+       'Мигрант-Потреб 2', 'Покупка и строит-во жилья', 'Товары в кредит',
+       'Корманд-кредит', 'Мигрант', 'Старт-бизнес',
+       'Зеленый кредит - Солнечные батареи', ' Жилье для сотрудников',
+       'Достижения', 'Сельхозкультура (Сароб)',
+       'Сельхозкультура-кредитная линия'],
+'pledge': ['Поручительство(Группа)', 'Недвижимость', 'Движимое имущество', 'Поручительство','Без залога'],
+'currency': ['Доллар США', 'Сомони', 'Рос.рубль']}
+nationality_encoded1 = pd.get_dummies(new_data['Nationality']).iloc[0, :]
+filial_encoded1 = pd.get_dummies(new_data['Filial']).iloc[0, :]
+region_encoded1 = pd.get_dummies(new_data['Region']).iloc[0, :]
+loan_goal_encoded1 = pd.get_dummies(new_data['loan_goal']).iloc[0, :]
+sector_encoded1 = pd.get_dummies(new_data['sector']).iloc[0, :]
+currency_encoded1 = pd.get_dummies(new_data['currency']).iloc[0, :]
+pledge_encoded1 = pd.get_dummies(new_data['pledge']).iloc[0, :]
 # Функции для предсказаний
-def issue_a_loan(Loan_amount, Loan_term, Days_of_delay, Number_of_delays, Lending_stage, Gross_profit, 
-                Net_profit, Age, Region_code, Direction_of_activity, business_experience):
+def issue_a_loan(Gender, FamilySize, Loan_amount, Loan_term, Repayment, Grace_preiod, Debt, Lending_stage,
+                 Net_profit, Age, FamilyStatus, Education, business_experiense, type_of_credit, has_overdue,
+                 high_debt, nationality_encoded, filial_encoded, region_encoded, loan_goal_encoded, 
+                 sector_encoded, currency_encoded, pledge_encoded):
     # Преобразование business_experience в числовой формат
-    business_experience = int(business_experience)
+    business_experience = int(business_experiense)
 
     # Преобразование всех переменных в числовой формат
-    prediction = classifier.predict([[float(Loan_amount), float(Loan_term), float(Days_of_delay), float(Number_of_delays), float(Lending_stage), float(Gross_profit), 
-                float(Net_profit), float(Age), float(Region_code), float(Direction_of_activity), float(business_experience)]])
-    
-    prediction2 = classifier.predict_proba([[float(Loan_amount), float(Loan_term), float(Days_of_delay), float(Number_of_delays), float(Lending_stage), float(Gross_profit), 
-                float(Net_profit), float(Age), float(Region_code), float(Direction_of_activity), float(business_experience)]])[:, 1]
-    
-    return prediction, prediction2
+    input_data = [
+        Gender, FamilySize, Loan_amount, Loan_term, Repayment, Grace_preiod, Debt, Lending_stage,
+        Net_profit, Age, FamilyStatus, Education, business_experience, type_of_credit, has_overdue,
+        high_debt
+    ]
+
+    # Добавляем закодированные значения для категориальных признаков
+    input_data += nationality_encoded.tolist()
+    input_data += filial_encoded.tolist()
+    input_data += region_encoded.tolist()
+    input_data += loan_goal_encoded.tolist()
+    input_data += sector_encoded.tolist()
+    input_data += currency_encoded.tolist()
+    input_data += pledge_encoded.tolist()
+
+    # Преобразуем в массив numpy и делаем предсказание
+    input_array = np.array(input_data).reshape(1, -1)
+    prediction1 = classifier1.predict(input_array)
+    prediction2 = classifier2.predict(input_array)
+    prediction3 = classifier3.predict(input_array)
+    total_pred = (prediction1 + prediction2 + prediction3) / 3
+    total_pred = total_pred.astype(int)
+    prediction2_1 = classifier1.predict_proba(input_array)
+    prediction2_2 = classifier2.predict_proba(input_array)
+    prediction2_3 = classifier3.predict_proba(input_array)
+    total_pred2 = (prediction2_1 + prediction2_2 + prediction2_3) / 3
+    total_pred2 = total_pred2.astype(int)
+    return total_pred, total_pred2
 
 
 
-def Delays_days(Loan_amount, Loan_term, Number_of_delays, Lending_stage, Gross_profit, 
-                Net_profit, Age, Region_code, Direction_of_activity, business_experience, issue):
-    reg1 = regression1.predict([[float(Loan_amount), float(Loan_term), float(Number_of_delays), float(Lending_stage), float(Gross_profit), 
-                float(Net_profit), float(Age), float(Region_code), float(Direction_of_activity), float(business_experience), float(issue)]])  
+
+def Delays_days(Gender, FamilySize, Loan_amount, Loan_term, Repayment, Grace_preiod, Debt, Lending_stage,
+                 Net_profit, Age, FamilyStatus, Education, business_experiense, type_of_credit, has_overdue,
+                 high_debt, nationality_encoded, filial_encoded, region_encoded, loan_goal_encoded, 
+                 sector_encoded, currency_encoded, pledge_encoded):
+    input_data = [
+        Gender, FamilySize, Loan_amount, Loan_term, Repayment, Grace_preiod, Debt, Lending_stage,
+        Net_profit, Age, FamilyStatus, Education, business_experience, type_of_credit, has_overdue,
+        high_debt
+    ]
+
+    # Добавляем закодированные значения для категориальных признаков
+    input_data += nationality_encoded.tolist()
+    input_data += filial_encoded.tolist()
+    input_data += region_encoded.tolist()
+    input_data += loan_goal_encoded.tolist()
+    input_data += sector_encoded.tolist()
+    input_data += currency_encoded.tolist()
+    input_data += pledge_encoded.tolist()
+
+    # Преобразуем в массив numpy и делаем предсказание
+    input_array = np.array(input_data).reshape(1, -1)
+    reg1 = regression1.predict(input_array)
     return reg1  
 
-def Credit_sum(Loan_amount, Loan_term, Days_of_delay, Number_of_delays, Lending_stage, Gross_profit, 
-                Net_profit, Age, Region_code, Direction_of_activity, business_experience):
-    reg2 = regression2.predict([[float(Loan_amount), float(Loan_term), float(Days_of_delay), float(Number_of_delays), float(Lending_stage), float(Gross_profit), 
-                float(Net_profit), float(Age), float(Region_code), float(Direction_of_activity), float(business_experience)]])  
+def Credit_sum(Gender, FamilySize, Loan_term, Repayment, Grace_preiod, Debt, Lending_stage,
+                 Net_profit, Age, FamilyStatus, Education, business_experiense, type_of_credit, has_overdue,
+                 high_debt, nationality_encoded, filial_encoded, region_encoded, loan_goal_encoded, 
+                 sector_encoded, currency_encoded, pledge_encoded, essue):
+    input_data = [
+        Gender, FamilySize, Loan_amount, Loan_term, Repayment, Grace_preiod, Debt, Lending_stage,
+        Net_profit, Age, FamilyStatus, Education, business_experience, type_of_credit, has_overdue,
+        high_debt, essue
+    ]
+
+    # Добавляем закодированные значения для категориальных признаков
+    input_data += nationality_encoded.tolist()
+    input_data += filial_encoded.tolist()
+    input_data += region_encoded.tolist()
+    input_data += loan_goal_encoded.tolist()
+    input_data += sector_encoded.tolist()
+    input_data += currency_encoded.tolist()
+    input_data += pledge_encoded.tolist()
+
+    # Преобразуем в массив numpy и делаем предсказание
+    input_array = np.array(input_data).reshape(1, -1)
+    reg2 = regression2.predict(input_array)    
     return reg2                  
                      
 # Основная функция
 def main():
+    sex = st.radio("Укажите свой пол:", ['Мужской', 'Женский'])
+    if sex=='Мужской':
+        Gender = 0
+    else:
+        Gender = 1
+        
+    selected_nationality = st.selectbox('Национальность:', nationality_encoded1)    
+    nationality_encoded += list(pd.get_dummies(new_data['Nationality'][selected_nationality]).iloc[0, :])
+    
     Age = st.number_input('Сколько вам полных лет?', step=1, value=0)
 
-    region_options = ['Шахристон', 'Гули сурх', 'Худжанд-Центр', 'Спитамен', 'Шарк', 'Мархамат', 'Душанбе', 'Навкент', 'Кистакуз',
-                  'Худжанд-Панчшанбе', 'Бустон', 'Истаравшан-филиал', 'Рудаки', 'Ашт', 'Калининобод', 'Сино', 'Исфара',
-                  'Хисор', 'Зафаробод', 'Ничони', 'Вахдат', 'Мехнатобод', 'Уяс', 'Дж.Расулов', 'Конибодом', 'Дусти',
-                  'Ниёзбек', 'Истаравшан', 'Рогун', 'Гончи', 'Чашмасор', 'Нофароч', 'Ободи', 'Каракчикум', 'Оббурдон',
-                  'Куруш', 'Ворух', 'Гулякандоз', 'Некфайз', 'Сомгор', 'Пунук', 'Панчакент', 'Кулканд', 'Оппон', 'Файзобод',
-                  'Турсунзода', 'Гусар', 'Равшан', 'Ифтихор', 'Х.Алиев', 'Ёри', 'Мучун', 'Саразм']
-
-    selected_region = st.selectbox('Регион\город проживания:', region_options)
+    family_options = ['Оиладор', 'Беоила', 'Бевамард (бевазан)', 'Чудошуда']
+    familyst = st.selectbox('Семейное положение:', family_options)
     label_encoder = LabelEncoder()
-    encoded_region = label_encoder.fit_transform(region_options)
-    Region_code = encoded_region[region_options.index(selected_region)]
+    encoded_family = label_encoder.fit_transform(family_options)
+    FamilyStatus = encoded_family[family_options.index(familyst)]
+    
+    FamilySize = st.number_input('Сколько человек в семье?', step=1, value=0)
+    
+    Age = st.number_input('Сколько вам полных лет?', step=1, value=0)
+    
+    educ =  ['Аспирантура', 'Оли', 'Миёнаи махсус', 'Олии нопурра', 'Миёна', 'Миёнаи нопурра']
+    education = st.selectbox('Уровень образования:', educ)
+    label_encoder = LabelEncoder()
+    encoded_educ = label_encoder.fit_transform(educ)
+    Education = encoded_educ[educ.index(education)]
+
+    type = st.selectbox('Тип кредита:', ['Потребительский кредит','Кредит на предпринимательскую деятельность'])
+    if type=='Потребительский кредит':
+        type_of_credit = 0
+    else:
+        type_of_credit = 1
+
+    selected_filial = st.selectbox('Филиал банка:', filial_encoded1)
+    filial_encoded += list(pd.get_dummies(new_data['Filial'][selected_filial]).iloc[0, :])
+    
+    selected_region = st.selectbox('Регион\город проживания:', region_encoded1)
+    region_encoded += list(pd.get_dummies(new_data['Region'][selected_region]).iloc[0, :])
+
+    selected_sector = st.selectbox('Сфера деятельности:', sector_encoded1)
+    sector_encoded += list(pd.get_dummies(new_data['sector'][selected_sector]).iloc[0, :])
+
+    selected_goal = st.selectbox('Цель кредита:', loan_goal_encoded1)
+    loan_goal_encoded += list(pd.get_dummies(new_data['loan_goal'][selected_goal]).iloc[0, :]) 
+
+    selected_pledge = st.selectbox('Тип залога:', pledge_encoded1)
+    pledge_encoded += list(pd.get_dummies(new_data['pledge'][selected_pledge]).iloc[0, :])  
+    
+    selected_currancy = st.selectbox('Тип валюты:', currency_encoded1)
+    currency_encoded += list(pd.get_dummies(new_data['currency'][selected_currancy]).iloc[0, :])
 
     Loan_amount = st.number_input('На какую сумму хотите взять кредит(в сомони)?', step=1, value=0) 
 
     Loan_term = st.number_input('На какой срок вы хотите взять кредит(месяц)?', step=1, value=0) 
 
-    activity_options = ['Животноводство и переработка молока', 'Приобретение техники', 'Ремонт дома', 'Торговля', 'Земледелие', 'Приобретение мебели', 'Оплата на лечение',
-                        'Проведение мероприятий', 'Оплата поездок', 'Услуги', 'Переоборудование транспорта', 'Потребнужды', 'Оплата образования', 'Производство', 
-                        'Покупка квартиры', 'Потреб.другое', 'Ремонт места деятельности', 'Сельское хозяйство', 'Все', 'Сушка фруктов', 'Коммерческий']
-    selected_activity = st.selectbox('Цель кредита:', activity_options)
-    label_encoder1 = LabelEncoder()
-    encoded_activity = label_encoder1.fit_transform(activity_options)
-    Direction_of_activity = encoded_activity[activity_options.index(selected_activity)]
-
     Lending_stage = st.number_input('Сколько кредитов вы брали(с учетом этого)?', step=1, value=0)
-    Number_of_delays = st.number_input('Сколько раз вы просрочили срок кредита?', step=1, value=0)
-    Days_of_delay = st.number_input('Сколько дней вы просрочили в последний раз, когда брали кредит?', step=1, value=0)
+    if Lending_stage>1:
+         have_delay = st.selectbox('Есть ли у вас просрочки?', ['Да','Нет'])
+         if have_delay == 'Да':
+             has_overdue = 1
+         else:
+             has_overdue = 0
+    else:
+        has_overdue = 0
+    Repayment = st.number_input('Ежемесячная сумма погашения:', step=1, value=0)
+    
+    Grace_preiod = st.number_input('Льготный период (месяц):', step=1, value=0)
+    
+    have_debt = st.selectbox('У вас есть долги?', ['Да','Нет'])
+    if have_debt == 'Да':
+        Debt = st.number_input('Введите сумму долга:', step=1, value=0)
+    else:
+        Debt = 0
+    high_debt = (Debt > 10000).astype(int)
     
     options = ['0-5', '5-10', '10-15', '15-20', '20-25', '25-40', '40-50', '50+']
     bus_exp =  st.radio("Стаж работы:", options)
@@ -83,35 +230,31 @@ def main():
     encoded_busEx = label_encoder.fit_transform(options)
     business_experience = encoded_busEx[options.index(bus_exp)]
 
-    
-    Gross_profit = st.number_input('Ваша валовая прибыль(разница между маржинальной прибылью и постоянными производственными расходами) в сомони(в месяц):', step=1, value=0)
-    Net_profit = st.number_input('Чистая прибыль в сомони(в месяц):', step=1, value=0)
+    Net_profit = st.number_input('Доход (в месяц):', step=1, value=0)
         
     result1 = ""
     result2 = ""
     result3 = ""
     result4 = ""
     if st.button("Predict"):
-        result1, result2 = issue_a_loan(Loan_amount, Loan_term, Days_of_delay, Number_of_delays, Lending_stage, Gross_profit, 
-                    Net_profit, Age, Region_code, Direction_of_activity, business_experience)
+        result1, result2 = issue_a_loan(Gender, FamilySize, Loan_amount, Loan_term, Repayment, Grace_preiod, Debt, Lending_stage,
+                 Net_profit, Age, FamilyStatus, Education, business_experiense, type_of_credit, has_overdue,
+                 high_debt, nationality_encoded, filial_encoded, region_encoded, loan_goal_encoded, 
+                 sector_encoded, currency_encoded, pledge_encoded)
         if result1 == 0:
-            if Number_of_delays == 0:
-                Number_of_delays = Number_of_delays+1
-                left =  Days_of_delay/Number_of_delays
-            else:  
-                left =  Days_of_delay/Number_of_delays
-            if left > 20:
-                st.success(f'К сожалению мы не можем выдать вам кредит(')
-            else:
-                result3 = Credit_sum(Loan_amount, Loan_term, Days_of_delay, Number_of_delays, Lending_stage, Gross_profit, 
-                Net_profit, Age, Region_code, Direction_of_activity, business_experience)
-                st.success(f'Сумму, которую вы ввели, пока что вам недоступно')
-                st.success(f'Максимальная сумма, которую мы можем вам выдать: {result3}')
+            result3 = Credit_sum(Gender, FamilySize, Loan_term, Repayment, Grace_preiod, Debt, Lending_stage,
+                 Net_profit, Age, FamilyStatus, Education, business_experiense, type_of_credit, has_overdue,
+                 high_debt, nationality_encoded, filial_encoded, region_encoded, loan_goal_encoded, 
+                 sector_encoded, currency_encoded, pledge_encoded, result1)
+            st.success(f'Сумму, которую вы ввели, пока что вам недоступно')
+            st.success(f'Максимальная сумма, которую мы можем вам выдать: {result3}')
         else:
             st.success(f'Кредит будет выдан с вероятностью {result1[0]*100:.2f}%')
             st.success(f'Вероятность возврата кредита вовремя: {result2[0]*100:.2f}%')
-            result4 = Delays_days(Loan_amount, Loan_term, Number_of_delays, Lending_stage, Gross_profit, 
-                    Net_profit, Age, Region_code, Direction_of_activity, business_experience, result1)
+            result4 = Delays_days(Gender, FamilySize, Loan_amount, Loan_term, Repayment, Grace_preiod, Debt, Lending_stage,
+                 Net_profit, Age, FamilyStatus, Education, business_experiense, type_of_credit, has_overdue,
+                 high_debt, nationality_encoded, filial_encoded, region_encoded, loan_goal_encoded, 
+                 sector_encoded, currency_encoded, pledge_encoded)
             st.success(f'Сколько примерно дней вы возможно просрочите: {(result4[0]).astype(int)}')
                                                    
 if __name__ == '__main__':
